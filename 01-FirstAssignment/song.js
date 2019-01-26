9d7ec4d0a1c2f3dda9fd7f0792e69025272b325d
Dileep Grandhi <grandhidileep@gmail.com>
2019-01-26 19:37:31 +0530

    Assignment #1

---
+++ 01-FirstAssignment/song.js
@@ -0,0 +1,27 @@
+//title of the song
+let songName = "Ringa Ringa";
+console.log(`songName ${songName}`);
+//album it belongs
+let album = "Arya 2";
+console.log(`album ${album}`);
+// music genre it belongs to
+let genre = "South Indian";
+console.log(`genre ${genre}`);
+// year in which song was released
+let releaseYear = 2009;
+console.log(`releaseYear ${releaseYear}`);
+// artists who composed the song
+let artists = ["Devi sri", "priya hemesh"];
+console.log(`artists ${artists}`);
+// duration of song in seconds
+let duration = 291;
+console.log(`duration ${duration}`);
+// language in which song is written
+let language = "telugu";
+console.log(`language ${language}`);
+// higher value of popularity indicate its more popular max value being 100
+let popularity = 50;
+console.log(`popularity ${popularity}/100`);
+// higher value of dancebility implies easier to dance max value being 100
+let dancebility = 50
+console.log(`dancebility ${dancebility}/100`);
